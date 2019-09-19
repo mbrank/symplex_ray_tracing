40864d37f6d7c5d88e0cc3d4085a85fd752a6050
Matic Brank <maticbrank@gmail.com>
2019-09-20 00:05:53 +0200

    Added classes for rays and convex polyhedrons.

---
+++ ComputeCrossSection.py
@@ -0,0 +1,9 @@
+class ComputeCrossSection(object):
+    """Documentation for ComputeCrossSection
+
+    """
+    def __init__(self, args):
+        super(ComputeCrossSection, self).__init__()
+        self.args = args
+
+    
---
+++ ConvexPolyhedron.py
@@ -0,0 +1,24 @@
+class ConvexPolyhedron(object):
+    """ConvexPolyhedron class
+
+    """
+    def __init__(self, pts):
+        super(ConvexPolyhedron, self).__init__()
+        self.pts = pts
+
+
+    def __showPointsInfo__(self):
+        for i in range(len(self.pts)):
+            print("Index:",i,"point:",
+                  self.pts[i][0],
+                  self.pts[i][1])
+
+
+    def addPoint(self, point):
+        self.pts.append(point)
+        
+        
+    def removePoint(self, index):
+        self.pts.pop(index)
+
+
